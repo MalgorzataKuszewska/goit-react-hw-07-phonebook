227d1ac66af13e127dbf60bfcf7d22231b6ae4d8
MalgorzataKuszewska <mkuszewska242@gmail.com>
2023-11-27 18:56:26 +0100

    Add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,36 @@
-export const App = () => {
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts, addContact, deleteContact } from 'redux/ContactsSlice';
+import { setFilter } from 'redux/FilterSlice';
+import ContactForm from 'components/ContactForm/ContactForm';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+const App = () => {
+  const dispatch = useDispatch();
+  const contacts = useSelector(state => state.contacts.data);
+  const filter = useSelector(state => state.filter);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const handleFilterChange = event => {
+    dispatch(setFilter(event.target.value.toLowerCase()));
+  };
+
+  const handleAddContact = newContact => {
+    dispatch(addContact(newContact));
+  };
+
+  const handleDeleteContact = contactId => {
+    dispatch(deleteContact(contactId));
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter)
+  );
+
   return (
     <div
       style={{
@@ -6,11 +38,25 @@ export const App = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        flexDirection: 'column',
+        fontSize: 24,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm
+        onAddContact={handleAddContact}
+        contacts={filteredContacts}
+      />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} onFilterChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
     </div>
   );
 };
+
+export default App;
